8143ad85e978d8be92cd4c037a93452c79ad8ac7
SerhiiBabych <sergeiy.babich@gmail.com>
2025-09-27 18:27:25 +0200

    Initial commit: techbob-frontend Next.js project

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,20 +28,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
          {/* Google Analytics 4 */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-J4DEVP6ZB7`}></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-J4DEVP6ZB7', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-        {/* End Google Analytics 4 */}
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-J4DEVP6ZB7"
+  ></script>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-J4DEVP6ZB7', { page_path: window.location.pathname });
+      `,
+    }}
+  />
+  {/* End Google Analytics 4 */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
