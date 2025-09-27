3d33d96511aa5d946720a8c86a557e97227e77e0
SerhiiBabych <sergeiy.babich@gmail.com>
2025-09-27 11:22:49 +0200

    Initial commit: techbob-frontend Next.js project

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-gray-900 text-white p-4">
       <div className="container mx-auto">
-        <a href="https://www.techbob.de"></a><h1 className="text-2xl font-bold">TechBOB</h1></a>
+        <a href="https://www.techbob.de"><h1 className="text-2xl font-bold">TechBOB</h1></a>
       </div>
     </header>
   );
