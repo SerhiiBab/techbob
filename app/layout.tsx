4e5abd2a26f18767fd7d4b2f2025eefa23141332
SerhiiBabych <sergeiy.babich@gmail.com>
2025-09-27 18:15:20 +0200

    Initial commit: techbob-frontend Next.js project

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,19 +27,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+         {/* Google Analytics 4 */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-J4DEVP6ZB7`}></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-12217370217');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-J4DEVP6ZB7', {
+                page_path: window.location.pathname,
+              });
             `,
           }}
         />
-        {/* End Google Tag Manager */}
+        {/* End Google Analytics 4 */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
