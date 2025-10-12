4ede2a812a0b2fd42860bdc715decc8ebc6c3fd0
SerhiiBabych <sergeiy.babich@gmail.com>
2025-10-12 13:29:35 +0200

    Initial commit: techbob-frontend Next.js project

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -14,7 +14,6 @@ export async function GET() {
     <url>
       <loc>https://www.techbob.de/news/${post.slug}</loc>
       <lastmod>${post.modified}</lastmod>
-      <changefreq>weekly</changefreq>
       <priority>0.8</priority>
     </url>
   `).join('');
@@ -23,7 +22,6 @@ export async function GET() {
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://www.techbob.de/</loc>
-      <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>
     ${urls}
